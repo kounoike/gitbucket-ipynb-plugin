42abec6ef654bbb738ae36a0a0364356aa683287
KOUNOIKE <kounoike.yuusuke@gmail.com>
2020-04-04 21:57:49 +0900

    bump version

--- build.sbt
+++ build.sbt
@@ -3,5 +3,5 @@ lazy val root = project in file(".")
 organization := "io.github.kounoike"
 name := "gitbucket-ipynb-plugin"
 version := "0.3.2"
-scalaVersion := "2.13.0"
-gitbucketVersion := "4.32.0"
+scalaVersion := "2.13.1"
+gitbucketVersion := "4.33.0"
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -17,7 +17,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("0.2.0"),
     new Version("0.2.1"),
     new Version("0.3.0"),
-    new Version("0.3.1")
+    new Version("0.3.1"),
+    new Version("0.3.2")
   )
 
   private[this] var renderer: Option[IpynbRenderer] = None
